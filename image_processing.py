f73680dfcb3aa6902c4b203335f44e3c7345472a
szainab <szainab@edu.uwaterloo.ca>
2018-11-20 13:42:16 -0500

    manual filter

--- image_processing.py
+++ image_processing.py
@@ -2,31 +2,28 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('pic.JPG',150)
-img2 = cv2.imread('dc_3rd.JPG',150)
+img = cv2.imread('tiny_img.jpg',0)
+# print(img)
+# img2 = cv2.imread('dc_3rd.JPG',150)
 
-blurred = cv2.GaussianBlur(img,(5,5),0)
-laplacian = cv2.Laplacian(blurred,cv2.CV_64F)
-blurred2 = cv2.GaussianBlur(img2,(5,5),0)
-laplacian2 = cv2.Laplacian(blurred2,cv2.CV_64F)
+# blurred = cv2.GaussianBlur(img,(5,5),0)
+laplacian = cv2.Laplacian(img,cv2.CV_64F)
 
-#sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
-#sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+newlaplacian = laplacian
+for i in range(len(laplacian)):
+    for j in range(len(laplacian)):
+        if laplacian[i][j] <= 60: #found this number by trial and error, need to find a way to do this for all maps
+            newlaplacian[i][j] = 1
+        else:
+            newlaplacian[i][j] = 0
+
+# for i in range(len(laplacian)):
+#     print(laplacian[i])
+
 
 plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
 plt.title('Original E5'), plt.xticks([]), plt.yticks([])
 plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
 plt.title('Laplacian E5'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,3),plt.imshow(img2,cmap = 'gray')
-plt.title('Original DC'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,4),plt.imshow(laplacian2,cmap = 'gray')
-plt.title('Laplacian DC'), plt.xticks([]), plt.yticks([])
-#plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
-#plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-#plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
-#plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
-
-#for i in range(len(sobelx)):
-#    print(laplacian[i][0], laplacian[i][1])
 
 plt.show()
